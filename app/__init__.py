84082c09eb90da4f4c3876c9315a2d9a3528e69a
datonix.jsx <104119840+Datonix-art@users.noreply.github.com>
2024-01-08 16:57:44 +0400

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,2 @@
 from extensions import app
-from routes import home, all_news, post, contact_us, signUp, logIn, logout, profile_information, view_info, delete, edit_info, edit_profile_password, edit_profile_username, inner_post, error_404
+from routes import home, post, contact_us, signUp, logIn, logout, profile_information, view_info, delete, edit_info, edit_profile_password, edit_profile_username, inner_post, error_404
